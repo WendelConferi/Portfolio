7fc752ff085619da10fa5ae10adf67f54a7272e6
Wendel Conferi <56461308+WendelConferi@users.noreply.github.com>
2022-09-15 15:42:30 -0300

    Update files :hammer:

--- js/app.js
+++ js/app.js
@@ -6,4 +6,16 @@ function stickyNavbar() {
   header.classList.toggle('scrolled', window.pageYOffset > 0)
 }
 
+stickyNavbar()
+
 window.addEventListener('scroll', stickyNavbar)
+
+/* ----------------- Reveal Animation ------------- */
+
+let sr = ScrollReveal({
+  duration: 2500,
+  distance: '60px'
+})
+
+sr.reveal('.showcase-info', { delay: 600 })
+sr.reveal('.showcase-image', { origin: 'top', delay: 700 })
