adbe88e65d96102e89bf82b3ca5dbfc915b7e8d5
Wendel Conferi <56461308+WendelConferi@users.noreply.github.com>
2022-09-16 19:15:10 -0300

    Update new sections

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 const header = document.querySelector('header')
 
+const first_skill = document.querySelector('.skill:first-child')
+const sk_counters = document.querySelectorAll('.counter span')
+const progress_bars = document.querySelectorAll('.skills svg circle')
+
+window.addEventListener('scroll', () => {
+  skillsCounter()
+})
+
 /* ----------------- STICKY BAR ------------- */
 
 function stickyNavbar() {
@@ -17,5 +25,21 @@ let sr = ScrollReveal({
   distance: '60px'
 })
 
-sr.reveal('.showcase-info', { delay: 600 })
-sr.reveal('.showcase-image', { origin: 'top', delay: 700 })
+sr.reveal('.showcase-info', { delay: 400 })
+sr.reveal('.showcase-image', { origin: 'top', delay: 500 })
+
+/* ----------------- Skills Progress Animation ------------- */
+
+function hasReached(el) {
+  let topPosition = el.getBoundingClientRect().top
+  if (window.innerHeight >= topPosition + el.offsetHeight) return true
+  return false
+}
+
+function skillsCounter() {
+  if (!hasReached(first_skill)) return
+
+  progress_bars.forEach(
+    p => (p.style.animation = 'progress 2s ease-in-out forwards')
+  )
+}
